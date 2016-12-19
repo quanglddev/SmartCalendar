3565f2445215f0246d36aa3a5fbb4ad91121f5a6
QUANG <crzQag@luongs-macbook-air.local>
2016-12-19 10:10:28 +0700

    Initial Commit

---
+++ Smart Calender (Beta)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart Calender (Beta)
+//
+//  Created by QUANG on 12/19/16.
+//  Copyright © 2016 Q.U.A.N.G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
